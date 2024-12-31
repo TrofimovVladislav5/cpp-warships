08cabd9aa5d2db9b66b2071e7e9f3ccbc8d75f46
Nikita Lapin <lapinnr_personal@proton.me>
2024-12-31 19:53:39 +0500

    fix: static template type on GameController object in main.cpp

--- controller/GameController.h
+++ controller/GameController.h
@@ -4,7 +4,6 @@
 #include "game-states/MenuGameState.h"
 #include "input-reader/InputReader.h"
 #include "input-reader/console/ConsoleInputReader.h"
-#include <type_traits>
 
 template <typename T>
 struct is_reader_derivative {
--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <library/defaults/Initials.h>
 #include "input-reader/InputReader.h"
 #include "GameController.h"
-#include "GameController.h"
 #include "input-reader/command/CommandInputReader.h"
-#include "input-reader/config/ConfigInputReader.h"
 
 int main(){
     Initials::consoleOutInitials();
     InputReader<>* inputReader = new CommandInputReader("../input-command.txt");
-    GameController controller(inputReader);
+    GameController<CommandInputReader> controller(inputReader);
     controller.run();
 
     return 0;
